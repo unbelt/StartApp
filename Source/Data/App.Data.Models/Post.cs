b6cd98e0f366a8f7702dc73963115bd802636da7
unbelt <unbelt@outlook.com>
2015-11-23 23:24:54 +0200

    Add basic models for the application

---
+++ Source/Data/App.Data.Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Data.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Source/Data/App.Data.Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace App.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+
+            return userIdentity;
+        }
+    }
+}
