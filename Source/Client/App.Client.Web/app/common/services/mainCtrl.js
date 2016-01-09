7181b069bf73efcdb6f1216e3815e4d1da9ed285
unbelt <unbelt@outlook.com>
2016-01-10 00:06:27 +0200

    [Client] Add Main controller

---
+++ Source/Client/App.Client.Web/app/common/services/mainCtrl.js
@@ -0,0 +1,15 @@
+﻿/******************
+ * Main Controller
+ ******************/
+(function () {
+    'use strict';
+
+    angular.module('app.controllers')
+        .controller('MainCtrl', MainCtrl);
+
+    function MainCtrl() {
+        var vm = this;
+
+        // TODO: Implement
+    }
+}());
